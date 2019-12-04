d4647529fe2de0152c931283a3ec0a88b0ada908
edevars <e.devars.dev@gmail.com>
2019-12-04 11:59:45 -0600

    :sparkles: Adding date to place schema

--- schemas/places.js
+++ schemas/places.js
@@ -1,4 +1,4 @@
-const joi = require('@hapi/joi');
+const joi = require('@hapi/joi').extend(require('@hapi/joi-date'));
 
 const placeIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const placeName = joi.string().max(50);
@@ -8,10 +8,11 @@ const placeDescription = joi
   .string()
   .min(120)
   .max(250);
-const placeAvailableSits = joi
-  .number().min(0)
+const placeAvailableSits = joi.number().min(0);
 
 const placeCost = joi.number();
+
+const placeDate = joi.date().format('DD-MM-YYYY');
 
 const createPlaceSchema = {
   name: placeName.required(),
@@ -19,7 +20,8 @@ const createPlaceSchema = {
   cover: placeCover.required(),
   description: placeDescription.required(),
   availableSits: placeAvailableSits.required(),
-  cost: placeCost.required()
+  cost: placeCost.required(),
+  date: placeDate.required()
 };
 
 const updatePlaceSchema = {
@@ -28,7 +30,8 @@ const updatePlaceSchema = {
   cover: placeCover,
   description: placeDescription,
   availableSits: placeAvailableSits,
-  cost: placeCost
+  cost: placeCost,
+  date: placeDate
 };
 
 module.exports = {
