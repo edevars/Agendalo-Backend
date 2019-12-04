ce564bbddddbe51333c12aeb7ad5eb123dacce13
edevars <e.devars.dev@gmail.com>
2019-12-03 20:48:08 -0600

    :sparkles: Adding a TDD test for util build message

---
+++ test/utils.buildMessage.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const buildMessage = require('../utils/buildMessage');
+
+describe.only('utils - buildMessage', () => {
+  describe('when recive an entity and and action', () => {
+    it('should return the respective message', () => {
+      const result = buildMessage('place', 'create');
+      const expect = 'place created';
+      assert.strictEqual(result, expect);
+    });
+  });
+
+  describe('when recive an entity and and action and is a list', () => {
+    it('should return the respective message with entity in plural', () => {
+      const result = buildMessage('place', 'list');
+      const expect = 'places listed';
+      assert.strictEqual(result, expect);
+    });
+  });
+});
